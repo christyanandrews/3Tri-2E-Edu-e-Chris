4d47ae78400e05a07544d5b85ea41a66c76c8ce7
christyanandrews <160140209+christyanandrews@users.noreply.github.com>
2024-10-28 08:52:07 -0300

    28/10

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function criaCartao(categoria,pergunta,resposta){
-    console.log(categoria,pergunta,resposta)
+ let conteniner = document.getElementById('conteiner')
+ let article = document.createElement('article')
+ article.className = 'cartao'
+ article.innerHTML = "teste foda"
+ conteniner.appendChild(article)
+
 }
 
 criaCartao("Biologia","Qual a função da mitocondria?","Produzir ATP energy yeah")
