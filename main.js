54a9809c44112808f16fc2e333514704fea00b4d
christyanandrews <160140209+christyanandrews@users.noreply.github.com>
2024-10-29 08:49:38 -0300

    29/10

--- main.js
+++ main.js
@@ -2,9 +2,20 @@ function criaCartao(categoria,pergunta,resposta){
  let conteniner = document.getElementById('conteiner')
  let article = document.createElement('article')
  article.className = 'cartao'
- article.innerHTML = "teste foda"
+ article.innerHTML = `
+  <div class="cartao-conteudo">
+  <h3> ${categoria} </h3>
+  <div class="cartao-pergunta1">
+  <p> ${pergunta} </p>
+  </div>
+  <div class="cartao-resposta1">
+  <p> ${resposta}</p>
+  </div>
+  </div>
+ `
  conteniner.appendChild(article)
 
 }
 
 criaCartao("Biologia","Qual a função da mitocondria?","Produzir ATP energy yeah")
+criaCartao("CIencias","Por que o Elon Musk foi nascido?","pois os XX e XY se conectaram")
